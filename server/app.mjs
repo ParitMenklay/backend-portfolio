38ba46b8c9b3bdae8bb89193349dd80732b5101a
Dragon <parit.menklay@gmail.com>
2026-02-02 23:47:17 +0700

    feat: initialize server package and database connection
    - Added package.json for server with dependencies including express, cors, dotenv, pg, and nodemon.
    - Created db.mjs to establish a connection pool using pg with DATABASE_URL from environment variables.

---
+++ server/app.mjs
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import express from "express"
+import cors from "cors";
+import connectionPool from "./utils/db.mjs";
+
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: [
+            process.env.FRONTEND_URL, 
+            // "https://your-frontend.vercel.app",   
+        ],
+    })
+);
+
+app.get("/test", (req, res) => {
+    return res.json("Server API is working 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "OK" });
+});
+
+app.get("/profiles", (req, res) => {
+    return res.json({
+        data: {
+            name: "john",
+            age: 20,
+        },
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+});
